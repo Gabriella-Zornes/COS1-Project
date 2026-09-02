dc9ac2b7d9a8faa4adfa0f283082ead7540ae6c0
Gabriella Zornes <GLZornes@student.fullsail.edu>
2026-09-01 20:51:53 -0400

    Forgot about EventManager.cpp, added the signitures to the cpp; seemed to fix error;

--- dev/COS1 Project/COS1 Project/EventManager.cpp
+++ dev/COS1 Project/COS1 Project/EventManager.cpp
@@ -1,1 +1,16 @@
 #include "EventManager.h"
+
+EventManager::EventManager()
+{
+	currentEvent = 0;
+}
+
+void EventManager::LoadEvents()
+{
+}
+
+void EventManager::Run()
+{
+}
+
+
--- dev/COS1 Project/COS1 Project/Game.cpp
+++ dev/COS1 Project/COS1 Project/Game.cpp
@@ -15,6 +15,10 @@ void Game::ShowMenu()
 
 }
 
+Game::Game()
+{
+}
+
 void Game::Run()
 {
 	int option = 0;
